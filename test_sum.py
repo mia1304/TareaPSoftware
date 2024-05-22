47dbbc4f5721710f0564b6378a3acf6006c18349
mia1304 <florestaumamaria@gmail.com>
2024-05-22 11:54:08 -0500

    añadir camibios

--- test_sum.py
+++ test_sum.py
@@ -9,6 +9,15 @@ class TestSum(unittest.TestCase):
     def test_sum_negative_numbers(self):
         self.assertEqual(sum(-1, -1), -2)
         self.assertEqual(sum(-1, 1), 0)
+        
+    def test_sum_zero(self):
+        self.assertEqual(sum(0, 0), 0)
+        self.assertEqual(sum(0, 5), 5)
+        self.assertEqual(sum(5, 0), 5)
+
+    def test_sum_floats(self):
+        self.assertAlmostEqual(sum(1.5, 2.5), 4.0)
+        self.assertAlmostEqual(sum(-1.1, 1.1), 0.0)
 
 
 if __name__ == '__main__':
